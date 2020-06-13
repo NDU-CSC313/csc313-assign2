45991a68e18592208f16dedbaeb8b718ac16e926
Hikmat Farhat <hfarhat@ndu.edu.lb>
2020-06-13 07:41:26 +0300

    startup code

--- main.cpp
+++ main.cpp
@@ -3,21 +3,6 @@
 #include <algorithm>
 #include <random>
 #include "TestClass.h"
-std::random_device e;
-std::uniform_int_distribution<> dist(1, 10);
-
-bool operator == (const TestClass& a,const TestClass & b){
-	return a._x == b._x;
-}
-void createTest() {
-
-	void* p = operator new(2*sizeof(TestClass));
-	TestClass* q = new(p) TestClass(7, 9);
-	TestClass* t = new(q+ sizeof(TestClass)) TestClass(11, 13);
-	q->~TestClass();
-	t->~TestClass();
-	operator delete(p);
-}
 
 /// @brief finds first occurence of val in range
 /// @tparam iterator 
@@ -28,20 +13,21 @@ void createTest() {
 /// @return iterator to the found element or end if not found
 template<typename iterator,typename T>
 iterator find(iterator begin, iterator end, const T& val) {
-	while (begin != end) {
-		if (*begin == val)return begin;
-		begin++;
-	}
-	return begin;
+//TODO: write your code here
+	return end;
 }
-
+/// @brief finds the first occurence when predicate returns true
+/// @tparam iterator 
+/// @tparam P 
+/// @param begin beginning of the range
+/// @param end end of the range
+/// @param pred supplied predicate
+/// @return iterator to the first such element or the end
 template<typename iterator, typename P>
 iterator find_if(iterator begin, iterator end, P pred) {
-	while (begin != end) {
-		if (pred(*begin))return begin;
-		begin++;
-	}
-	return begin;
+	//TODO: write your code here
+
+	return end;
 }
 
 /// @brief 
@@ -53,65 +39,83 @@ iterator find_if(iterator begin, iterator end, P pred) {
 /// @return 
 template <typename iterator,typename T>
 iterator remove(iterator begin,iterator end, T val) {
-     begin = ::find(begin, end, val);
-	if(begin!=end)
-		for(iterator itr=begin+1;itr!=end;itr++){
-			if (*itr != val) {
-				*begin = std::move(*itr);
-				begin++;
-			}
-
-		}
-	return begin;
+    //TODO: write your code here
+	return end;
 }
+
 template<typename iterator, typename P>
 iterator remove_if(iterator begin, iterator end, P pred) {
-	begin = ::find_if(begin, end, pred);
-	if (begin != end)
-		for (iterator itr = begin + 1; itr != end; itr++) {
-			if (!pred(*itr)) {
-				*begin = std::move(*itr);
-				begin++;
-			}
-
-		}
-	return begin;
+	//TODO: write your code here
+	return end;
 }
 int main(){
-	
-	
-	/*	std::cout << "array\n";
-		TestClass t[5];
-		TestClass* p = (TestClass *) operator new (5 * sizeof(TestClass));
-		new(p) TestClass(1, 2);
-		new(p + 1) TestClass(3, 4);
-		new (p + 2) TestClass(5, 6);
-		new (p + 3) TestClass(7, 8);
-		new (p + 4) TestClass(9, 10);
-
-		int x = 12;*/
-	//TODO: example about adjacent_find
-	//std::adjacent_find;
-
-	//TODO: example about transofrm
-	//std::transform;
+	std::cout<<"exercise 1: implement find(). 20 pts\n\
+as an EXAMPLE, the code below should output\n\
+4,5,5,4,3,2,1,0,\n\
+-------------------\n";
 	{
-		const int n = 10;
-		std::random_device e;
-		std::uniform_int_distribution<> dist(1, 10);
-		std::vector<int> v(10);
-		std::generate(v.begin(), v.end(), [&]() {return dist(e); });
-		
+		std::vector<int> v{ 1,2,3,4,5,5,4,3,2,1,0 };
+		auto itr = ::find(v.begin(), v.end(), 4);
+		std::cout << "output\n";
+		for (auto i = itr; i != v.end(); ++i)
+			std::cout << *i << ",";
+		std::cout << std::endl;
+		std::cout<<"----------------------\n";
+		std::cout<<"----------------------\n";
+
+	}
+	std::cout << "exercise 2: implement find_if(). 20 pts\n\
+as an EXAMPLE, the code below should output\n\
+5,5,4,3,2,1,0,\n\
+-------------------\n";
+	{
+		std::vector<int> v{ 1,2,3,4,5,5,4,3,2,1,0 };
+		auto itr = ::find_if(v.begin(), v.end(), [](int n) {return ((n % 2 == 1) && n > 3); });
+		std::cout << "output\n";
+		for (auto i = itr; i != v.end(); ++i)
+			std::cout << *i << ",";
+		std::cout << std::endl;
+		std::cout<<"----------------------\n";
+		std::cout<<"----------------------\n";
+	}
+	std::cout << "exercise 3: implement remove(). 20 pts\n\
+as an EXAMPLE, the code below should output\n\
+1,2,4,5,5,4,2,1,0,1,0,\n\
+1,2,4,5,5,4,2,1,0,\n\
+-------------------\n";
+	{
+		std::vector<int> v{ 1,2,3,4,5,5,4,3,2,1,0 };
+		auto itr = ::remove(v.begin(), v.end(), 3);
+		std::cout << "output\n";
+		for (auto& x:v)
+			std::cout << x << ",";
+		std::cout << std::endl;
+		v.erase(itr, v.end());
 		for (auto& x : v)
 			std::cout << x << ",";
 		std::cout << std::endl;
+		std::cout << "----------------------\n";
+		std::cout << "----------------------\n";
+
+	}
+	std::cout << "exercise 4: implement remove_if(). 20 pts\n\
+as an EXAMPLE, the code below should output\n\
+1,3,5,5,3,1,4,3,2,1,0,\n\
+1,3,5,5,3,1,\n\
+-------------------\n";
+	{
+		std::vector<int> v{ 1,2,3,4,5,5,4,3,2,1,0 };
 		auto itr = ::remove_if(v.begin(), v.end(), [](int n) { return (n % 2 == 0); });
-		std::cout << "print from first event to end\n";
+		std::cout << "output\n";
+		for (auto& x : v)
+			std::cout << x << ",";
+		std::cout << std::endl;
 		v.erase(itr, v.end());
-		for (auto i = v.begin(); i != v.end(); ++i)
-			std::cout << *i << ",";
+		for (auto& x:v)
+			std::cout << x << ",";
 
 		std::cout << std::endl;
-
+		std::cout << "----------------------\n";
+		std::cout << "----------------------\n";
 	}
 }
